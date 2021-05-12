76a612af500276cbe6843c549e611cc7236bc3e5
Amlan <amlannandy5@gmail.com>
2021-05-12 12:58:14 +0530

    Created custom button widget

--- app/src/main/java/com/aknindustries/chatify/activities/SplashActivity.kt
+++ app/src/main/java/com/aknindustries/chatify/activities/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.aknindustries.chatify.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aknindustries.chatify.R
@@ -8,5 +9,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        goToLogin()
+    }
+
+    private fun goToLogin() {
+        intent = Intent(this@SplashActivity, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
